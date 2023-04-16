9390844ff47a538f89395e417465f6342ca9ad7a
Paras <paras17250@iiitd.ac.in>
2023-04-16 11:38:45 +0530

    matplotl added matplotlibcpp.h

--- pgrrt_3D/config.h
+++ pgrrt_3D/config.h
@@ -5,9 +5,9 @@
 #ifndef UNTITLED2_CONFIG_H
 #define UNTITLED2_CONFIG_H
 
-#define RESOLUTION 30.
+#define RESOLUTION 90.
 #define SP_THRESHOLD 40.
-#define MAX_STEER_ANGLE 50.
+#define MAX_STEER_ANGLE 100.
 #define STEP_SIZE .5f
 #define print_file true
 
